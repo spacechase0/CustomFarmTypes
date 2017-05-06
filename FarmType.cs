be58498f3d9624a4b4aa5ad1bdca3991e6c1d465
Chase W <chasew@silkweaver.com>
2017-05-06 17:09:35 -0400

    Initial commit. Probably should have done this a lot sooner.

---
+++ FarmType.cs
@@ -0,0 +1,414 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using xTile;
+using Newtonsoft.Json;
+using SObject = StardewValley.Object;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CustomFarmTypes
+{
+    public class FarmType
+    {
+        public class FarmBehavior
+        {
+            public class SpawnBehaviorArea
+            {
+                public double Chance { get; set; } = 1.0;
+                public MyRectangle Area { get; set; }
+                public List<SpawnBehaviorEntry> Entries { get; set; } = new List<SpawnBehaviorEntry>();
+
+                public SpawnBehaviorArea() { }
+                public SpawnBehaviorArea(MyRectangle area)
+                {
+                    Area = area;
+                }
+            }
+
+            // This work as sequential ifs. No elses
+            public class SpawnBehaviorEntry
+            {
+                public double Chance { get; set; } = 0;
+                public double LuckFactor { get; set; } = 0;
+                public bool SkipChanceDecrease { get; set; } = false;
+
+                public bool OnlyTryIfNoneSelectedYet { get; set; } = false;
+
+                // Option A: A single choice
+                public int MiningLevelRequirement { get; set; } = -1;
+                public int ObjectID { get; set; }
+                public int InitialStack { get; set; } = 1;
+                public int OreHealth { get; set; } = 0;
+
+                // Option B: More behavior entries, except these act as if, else if.
+                // The last option will always be chosen if the others aren't.
+                public List<SpawnBehaviorEntry> SubEntries { get; set; }
+
+                public SpawnBehaviorEntry()
+                {
+                }
+
+                public SObject getObject()
+                {
+                    if (SubEntries != null)
+                    {
+                        foreach (var entry in SubEntries)
+                        {
+                            if (Game1.random.NextDouble() < entry.Chance)
+                                return entry.getObject();
+                        }
+                        return SubEntries.Last().getObject();
+                    }
+
+                    return new SObject(new Vector2(), ObjectID, InitialStack) { minutesUntilReady = OreHealth };
+                }
+
+                public static SpawnBehaviorEntry Forageable(double chance, int obj)
+                {
+                    SpawnBehaviorEntry s = new SpawnBehaviorEntry();
+                    s.Chance = chance;
+                    s.ObjectID = obj;
+                    return s;
+                }
+
+                public static SpawnBehaviorEntry Ore(double chance, int levelReq, int obj, int health)
+                {
+                    SpawnBehaviorEntry s = new SpawnBehaviorEntry();
+                    s.Chance = chance;
+                    s.MiningLevelRequirement = levelReq;
+                    s.ObjectID = obj;
+                    s.InitialStack = 10;
+                    s.OreHealth = health;
+                    return s;
+                }
+
+                public static SpawnBehaviorEntry chooseEntry(List<SpawnBehaviorEntry> entries)
+                {
+                    SpawnBehaviorEntry ret = null;
+                    foreach (var entry in entries)
+                    {
+                        if (entry.OnlyTryIfNoneSelectedYet && ret != null)
+                            continue;
+
+                        if (Game1.random.NextDouble() <= entry.Chance)
+                            ret = entry;
+                    }
+                    return ret == null ? entries.Last() : ret;
+                }
+            }
+
+            // Fishing
+            public class FishPoolDrawEntry
+            {
+                public MyRectangle Area { get; set; }
+                public int PoolId { get; set; } = 0;
+                public int ListFromLocationID { get; set; } = -1;
+
+                public FishPoolDrawEntry() { }
+                public FishPoolDrawEntry(MyRectangle area, int id, int list)
+                {
+                    Area = area;
+                    PoolId = id;
+                    ListFromLocationID = list;
+                }
+
+            }
+            public class FishPoolEntry
+            {
+                public double Chance { get; set; }
+                public double LuckFactor { get; set; } = 0;
+
+                public bool OnlyTryIfNoneSelectedYet { get; set; } = false;
+
+                // Option A: Just use the fish type from another location
+                public string LocationPreset { get; set; }
+
+                // Option B: A specific object
+                public int ObjectID { get; set; }
+
+                public static FishPoolEntry FinalPreset(string loc)
+                {
+                    FishPoolEntry fish = new FishPoolEntry();
+                    fish.Chance = 1.0;
+                    fish.OnlyTryIfNoneSelectedYet = true;
+                    fish.LocationPreset = loc;
+                    return fish;
+                }
+
+                public static FishPoolEntry Preset(double chance, string loc, double luck = 0)
+                {
+                    FishPoolEntry fish = new FishPoolEntry();
+                    fish.Chance = chance;
+                    fish.LuckFactor = luck;
+                    fish.LocationPreset = loc;
+                    return fish;
+                }
+
+                public static FishPoolEntry FinalObject(int obj)
+                {
+                    FishPoolEntry fish = new FishPoolEntry();
+                    fish.Chance = 1.0;
+                    fish.OnlyTryIfNoneSelectedYet = true;
+                    fish.ObjectID = obj;
+                    return fish;
+                }
+
+                public static FishPoolEntry Object(double chance, int obj, double luck = 0)
+                {
+                    FishPoolEntry fish = new FishPoolEntry();
+                    fish.Chance = chance;
+                    fish.LuckFactor = luck;
+                    fish.ObjectID = obj;
+                    return fish;
+                }
+
+                public SObject getObject()
+                {
+                    return new SObject(ObjectID, 1);
+                }
+
+                public static FishPoolEntry chooseEntry(List<FishPoolEntry> entries)
+                {
+                    FishPoolEntry ret = null;
+                    foreach (var entry in entries)
+                    {
+                        if (entry.OnlyTryIfNoneSelectedYet && ret != null)
+                            continue;
+
+                        if (Game1.random.NextDouble() < entry.Chance + Game1.dailyLuck * entry.LuckFactor)
+                            ret = entry;
+                    }
+                    return ret;
+                }
+            }
+            public double FishingSplashChance { get; set; }
+            public List<FishPoolDrawEntry> FishPoolToDrawFrom;
+            public List<List<FishPoolEntry>> FishPools;
+
+            // Foraging
+            public bool RepopulateStumps { get; set; }
+            public int SpecialWeedCount { get; set; }
+            public double ForageableSpawnChanceBase { get; set; }
+            public double ForageableSpawnChanceMultiplier { get; set; }
+            public Dictionary<string, List<SpawnBehaviorArea>> ForageableSpawnBehavior { get; set; }
+
+            // Mining
+            public int NewSaveOreGenRuns { get; set; }
+            public double OreSpawnChanceBase { get; set; }
+            public double OreSpawnChanceMultiplier { get; set; }
+            public List<SpawnBehaviorArea> OreSpawnBehavior { get; set; }
+
+            // Combat
+            public bool SpawnMonsters { get; set; }
+
+            // ...
+            public static SpawnBehaviorArea chooseSpawnArea(List<SpawnBehaviorArea> areas)
+            {
+                foreach (var area in areas)
+                {
+                    if (Game1.random.NextDouble() <= area.Chance)
+                        return area;
+                }
+                return areas.Last();
+            }
+        }
+
+        [JsonIgnore]
+        public string Folder { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; } = "";
+        public string ID { get; set; }
+
+        [JsonIgnore]
+        public virtual Texture2D Icon
+        {
+            get
+            {
+                return Mod.instance.Helper.Content.Load<Texture2D>(Folder + "/icon.png", ContentSource.ModFolder);
+            }
+        }
+
+        // Only valid for farms using CustomFarm
+        public int BehaviorPreset { get; set; } = Farm.default_layout;
+        public FarmBehavior Behavior
+        {
+            get
+            {
+                if (behavior_ == null)
+                    behavior_ = getFarmBehaviorFromPreset(BehaviorPreset);
+                return behavior_;
+            }
+            set { behavior_ = value; }
+        }
+        private FarmBehavior behavior_;
+
+        // TODO: House furniture and stuff
+        
+        public virtual Map loadMap()
+        {
+            return Mod.instance.Helper.Content.Load<Map>( Folder + "/map.xnb", ContentSource.ModFolder);
+        }
+
+        public virtual Farm getFarm( string loc )
+        {
+            return new CustomFarm(this, loc);
+        }
+
+        public static FarmType getFarmTypeFromPreset( int vanillaId )
+        {
+            FarmType f = new FarmType();
+            f.Name = Farm.getMapNameFromTypeInt(vanillaId);
+            f.ID = "StardewValley." + f.Name;
+            f.BehaviorPreset = vanillaId;
+
+            return f;
+        }
+
+        public static FarmBehavior getFarmBehaviorFromPreset(int vanillaId)
+        {
+            MyRectangle mapRect = new MyRectangle(0, 0, 80, 65);
+
+            FarmBehavior b = new FarmBehavior();
+            switch (vanillaId)
+            {
+                case Farm.default_layout:
+                    break;
+                case Farm.riverlands_layout:
+                    {
+                        b.FishingSplashChance = 0.5;
+                        b.FishPoolToDrawFrom = new List<FarmBehavior.FishPoolDrawEntry>();
+                        b.FishPoolToDrawFrom.Add(new FarmBehavior.FishPoolDrawEntry(mapRect, 0, 1));
+                        var fish = new List<FarmBehavior.FishPoolEntry>();
+                        fish.Add(FarmBehavior.FishPoolEntry.Preset(0.3, "Forest"));
+                        fish.Add(FarmBehavior.FishPoolEntry.FinalPreset("Town"));
+                        b.FishPools = new List<List<FarmBehavior.FishPoolEntry>>();
+                        b.FishPools.Add(fish);
+                    }
+                    break;
+                case Farm.forest_layout:
+                    {
+                        b.FishPoolToDrawFrom = new List<FarmBehavior.FishPoolDrawEntry>();
+                        b.FishPoolToDrawFrom.Add(new FarmBehavior.FishPoolDrawEntry(mapRect, 0, 1));
+                        var fish = new List<FarmBehavior.FishPoolEntry>();
+                        fish.Add(FarmBehavior.FishPoolEntry.Object(0.5, 734, 1.0));
+                        fish.Add(FarmBehavior.FishPoolEntry.Preset(0.5, "Forest"));
+                        b.FishPools = new List<List<FarmBehavior.FishPoolEntry>>();
+                        b.FishPools.Add(fish);
+
+                        b.RepopulateStumps = true;
+                        b.SpecialWeedCount = 6;
+
+                        b.ForageableSpawnChanceBase = 0.75;
+                        b.ForageableSpawnChanceMultiplier = 1.0;
+                        b.ForageableSpawnBehavior = new Dictionary<string, List<FarmBehavior.SpawnBehaviorArea>>();
+
+                        var spring = new FarmBehavior.SpawnBehaviorArea(mapRect);
+                        spring.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 16));
+                        spring.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 22));
+                        spring.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 20));
+                        spring.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.00, 257)); // The last one is always chosen if none of the others are. No need to give it an extra 25% chance as well.
+                        var springAreas = new List<FarmBehavior.SpawnBehaviorArea>();
+                        springAreas.Add(spring);
+                        b.ForageableSpawnBehavior.Add("spring", springAreas);
+
+                        var summer = new FarmBehavior.SpawnBehaviorArea(mapRect);
+                        summer.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 402));
+                        summer.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 396));
+                        summer.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 398));
+                        summer.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.00, 404));
+                        var summerAreas = new List<FarmBehavior.SpawnBehaviorArea>();
+                        summerAreas.Add(summer);
+                        b.ForageableSpawnBehavior.Add("summer", summerAreas);
+
+                        var fall = new FarmBehavior.SpawnBehaviorArea(mapRect);
+                        fall.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 281));
+                        fall.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 420));
+                        fall.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.25, 422));
+                        fall.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Forageable(0.00, 404));
+                        var fallAreas = new List<FarmBehavior.SpawnBehaviorArea>();
+                        fallAreas.Add(fall);
+                        b.ForageableSpawnBehavior.Add("fall", fallAreas);
+                    }
+                    break;
+                case Farm.mountains_layout:
+                    {
+                        b.FishPoolToDrawFrom = new List<FarmBehavior.FishPoolDrawEntry>();
+                        b.FishPoolToDrawFrom.Add(new FarmBehavior.FishPoolDrawEntry(mapRect, 0, 1));
+                        var fish = new List<FarmBehavior.FishPoolEntry>();
+                        fish.Add(FarmBehavior.FishPoolEntry.Preset(0.5, "Forest"));
+                        b.FishPools = new List<List<FarmBehavior.FishPoolEntry>>();
+                        b.FishPools.Add(fish);
+
+                        b.OreSpawnChanceBase = 1;
+                        b.OreSpawnChanceMultiplier = 0.66;
+                        b.NewSaveOreGenRuns = 28;
+
+                        var chances = new FarmBehavior.SpawnBehaviorArea(new MyRectangle(5, 37, 22, 8));
+                        var firstChance = FarmBehavior.SpawnBehaviorEntry.Ore(.15, -1, 590, 0);
+                        firstChance.SkipChanceDecrease = true;
+                        firstChance.InitialStack = 1;
+                        chances.Entries.Add(firstChance);
+                        chances.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.5, -1, 670, 0));
+                        var multiChance = new FarmBehavior.SpawnBehaviorEntry();
+                        multiChance.Chance = .1;
+                        multiChance.SubEntries = new List<FarmBehavior.SpawnBehaviorEntry>();
+                        multiChance.SubEntries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.33, 8, 77, 7));
+                        multiChance.SubEntries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.5, 5, 76, 5));
+                        multiChance.SubEntries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(1, -1, 75, 3));
+                        chances.Entries.Add(multiChance);
+                        chances.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.21, -1, 751, 3));
+                        chances.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.15, 4, 290, 4));
+                        chances.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.1, 7, 764, 8));
+                        chances.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(.01, 10, 765, 16));
+                        chances.Entries.Add(FarmBehavior.SpawnBehaviorEntry.Ore(1.0, -1, 668, 2));
+
+                        b.OreSpawnBehavior = new List< FarmBehavior.SpawnBehaviorArea >();
+                        b.OreSpawnBehavior.Add(chances);
+                    }
+                    break;
+                case Farm.combat_layout:
+                    {
+                        b.FishPoolToDrawFrom = new List<FarmBehavior.FishPoolDrawEntry>();
+                        b.FishPoolToDrawFrom.Add(new FarmBehavior.FishPoolDrawEntry(mapRect, 0, 0));
+                        var fish = new List<FarmBehavior.FishPoolEntry>();
+                        fish.Add(FarmBehavior.FishPoolEntry.Preset(0.35, "Mountain"));
+                        b.FishPools = new List<List<FarmBehavior.FishPoolEntry>>();
+                        b.FishPools.Add(fish);
+
+                        b.SpawnMonsters = true;
+                    }
+                    break;
+            }
+
+            return b;
+        }
+
+        private static Dictionary<string, FarmType> types = new Dictionary<string, FarmType>();
+
+        public static void register( FarmType type )
+        {
+            if ( types.ContainsKey( type.ID ) )
+            {
+                Log.error("Type \"" + type.ID + "\" already registered.");
+                return;
+            }
+            types.Add(type.ID, type);
+        }
+
+        public static FarmType getType( string id )
+        {
+            return types.ContainsKey( id ) ? types[id] : null;
+        }
+
+        public static List< FarmType > getTypes()
+        {
+            return types.Values.ToList();
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using SFarmer = StardewValley.Farmer;
+using System.Collections.Generic;
+using StardewValley.Menus;
+
+namespace CustomFarmTypes
+{
+    public class Mod : StardewModdingAPI.Mod
+    {
+        public const int MY_FARM_TYPE = 6;
+
+        public static Mod instance;
+        public static SaveData Data = new SaveData();
+
+        public override void Entry(IModHelper helper)
+        {
+            base.Entry(helper);
+            instance = this;
+            
+            SaveEvents.AfterLoad += afterLoad;
+            SaveEvents.BeforeSave += beforeSave;
+            SaveEvents.AfterSave += afterSave;
+            GameEvents.UpdateTick += onUpdate;
+
+            /*
+            var fish = FarmType.getFarmBehaviorFromPreset(Farm.riverlands_layout);
+            var forage = FarmType.getFarmBehaviorFromPreset(Farm.forest_layout);
+            var mine = FarmType.getFarmBehaviorFromPreset(Farm.mountains_layout);
+            var fight = FarmType.getFarmBehaviorFromPreset(Farm.combat_layout);
+
+            FarmType.FarmBehavior b = new FarmType.FarmBehavior();
+            b.FishingSplashChance = fish.FishingSplashChance;
+            b.FishPools = fish.FishPools;
+            b.FishPoolToDrawFrom = fish.FishPoolToDrawFrom;
+            b.FishPools[0].Insert(0, forage.FishPools[0][0]);
+            b.RepopulateStumps = forage.RepopulateStumps;
+            b.SpecialWeedCount = forage.SpecialWeedCount;
+            b.ForageableSpawnBehavior = forage.ForageableSpawnBehavior;
+            b.ForageableSpawnChanceBase = forage.ForageableSpawnChanceBase;
+            b.ForageableSpawnChanceMultiplier = forage.ForageableSpawnChanceMultiplier;
+            b.NewSaveOreGenRuns = mine.NewSaveOreGenRuns;
+            b.OreSpawnBehavior = mine.OreSpawnBehavior;
+            b.OreSpawnBehavior[0].Area = new MyRectangle(52, 50, 11, 11);
+            b.OreSpawnChanceBase = mine.OreSpawnChanceBase;
+            b.OreSpawnChanceMultiplier = mine.OreSpawnChanceMultiplier;
+            b.SpawnMonsters = fight.SpawnMonsters;
+
+            FarmType o = new FarmType();
+            o.Name = "Everything";
+            o.Description = "A farm incorporating features from every type, located in the forest.";
+            o.ID = "spacechase0.FarmType.Everything";
+            o.BehaviorPreset = 0;
+            o.Behavior = b;
+            Helper.WriteJsonFile(Path.Combine(Helper.DirectoryPath, "everything.json"), o);
+            //*/
+
+            compileChoices();
+        }
+
+        private void afterLoad(object sender, EventArgs args)
+        {
+            if (!(Game1.year == 1 && Game1.currentSeason == "spring" && Game1.dayOfMonth == 0))
+            {
+                Log.debug("Loading save data... " + Game1.year + " " + Game1.currentSeason + " " + Game1.dayOfMonth);
+                Data = Helper.ReadJsonFile<SaveData>(Path.Combine(Constants.CurrentSavePath, "custom-farm-type.json"));
+                if (Data == null)
+                {
+                    Data = new SaveData();
+                    return;
+                }
+            }
+
+            foreach ( var entry in Data.FarmTypes )
+            {
+                GameLocation loc = Game1.getLocationFromName( entry.Key );
+                if ( loc.GetType() != typeof( Farm ) )
+                    continue;
+
+                Log.info("Custom farm type " + entry.Value + " for " + entry.Key);
+                FarmType type = FarmType.getType(entry.Value);
+                if (type == null)
+                {
+                    Log.error("Custom farm type is missing!");
+                    return;
+                }
+
+                var newFarm = new CustomFarm(type, loc.name);
+                if (Game1.year == 1 && Game1.currentSeason == "spring" && Game1.dayOfMonth == 0)
+                {
+                    Log.debug("First day? from load");
+                    for (int index = 0; index < type.Behavior.NewSaveOreGenRuns; ++index)
+                        newFarm.doOreSpawns();
+                }
+                else CustomFarm.swapFarms(loc as Farm, newFarm);
+                Game1.locations.Remove(loc);
+                Game1.locations.Add(newFarm);
+            }
+        }
+
+        private void beforeSave(object sender, EventArgs args)
+        {
+            Log.info("Before save, use vanilla farms...");
+
+            foreach (var entry in Data.FarmTypes)
+            {
+                GameLocation loc = Game1.getLocationFromName(entry.Key);
+                if (loc.GetType() == typeof(CustomFarm))
+                {
+                    Log.debug("Putting vanilla farm over " + entry.Key + "=" + entry.Value);
+                    var farm = loc as CustomFarm;
+                    Farm newFarm = new Farm(Helper.Content.Load<xTile.Map>("Maps\\Farm", ContentSource.GameContent), loc.name);
+                    CustomFarm.swapFarms(farm, newFarm);
+                    Game1.locations.Remove(farm);
+                    Game1.locations.Add(newFarm);
+                }
+            }
+        }
+
+        private void afterSave(object sender, EventArgs args)
+        {
+            Log.info("Saving, putting custom farms back...");
+            Helper.WriteJsonFile(Path.Combine(Constants.CurrentSavePath, "custom-farm-type.json"), Data);
+            foreach (var entry in Data.FarmTypes)
+            {
+                GameLocation loc = Game1.getLocationFromName(entry.Key);
+                if (loc.GetType() != typeof(Farm))
+                    continue;
+
+                Log.debug("Putting custom farm back for " + entry.Key + "=" + entry.Value);
+                FarmType type = FarmType.getType(entry.Value);
+                if (type == null)
+                {
+                    Log.error("Custom farm type is missing!");
+                    return;
+                }
+
+                var newFarm = new CustomFarm(type, loc.name);
+                if ( Game1.year == 1 && Game1.currentSeason == "spring" && Game1.dayOfMonth == 1 )
+                {
+                    Log.debug("First day? from save");
+                    for (int index = 0; index < type.Behavior.NewSaveOreGenRuns; ++index)
+                        newFarm.doOreSpawns();
+                }
+                else CustomFarm.swapFarms(loc as Farm, newFarm);
+                Game1.locations.Remove(loc);
+                Game1.locations.Add(newFarm);
+            }
+        }
+
+        private IClickableMenu prevMenu = null;
+        private void onUpdate(object sender, EventArgs args)
+        {
+            if (Game1.activeClickableMenu is TitleMenu)
+            {
+                if (TitleMenu.subMenu is CharacterCustomization)
+                {
+                    Log.debug("Found vanilla new game window, replacing with our own.");
+
+                    var oldMenu = (CharacterCustomization)TitleMenu.subMenu;
+                    var shirts = Helper.Reflection.GetPrivateValue<List<int>>(oldMenu, "shirtOptions");
+                    var hairs = Helper.Reflection.GetPrivateValue<List<int>>(oldMenu, "hairStyleOptions");
+                    var accessories = Helper.Reflection.GetPrivateValue<List<int>>(oldMenu, "accessoryOptions");
+                    var wizard = Helper.Reflection.GetPrivateValue<bool>(oldMenu, "wizardSource");
+                    var newMenu = new NewCharacterCustomizeMenu(shirts, hairs, accessories, wizard);
+
+                    TitleMenu.subMenu = newMenu;
+                }
+            }
+            prevMenu = Game1.activeClickableMenu;
+        }
+
+        private void compileChoices()
+        {
+            Log.info("Creating list of custom farm types...");
+            var choices = Directory.GetDirectories(Path.Combine(Helper.DirectoryPath, "FarmTypes"));
+            foreach (var choice in choices)
+            {
+                if (!File.Exists(Path.Combine(choice, "type.json")) || !File.Exists(Path.Combine(choice, "map.xnb")) || !File.Exists(Path.Combine(choice, "icon.png")) )
+                {
+                    Log.error("A required file is missing for custom farm type \"" + choice + "\".");
+                    continue;
+                }
+
+                FarmType type = Helper.ReadJsonFile<FarmType>(Path.Combine(choice, "type.json"));
+                if ( type == null )
+                {
+                    Log.error("Problem reading type.json for custom farm type \"" + choice + "\".");
+                    continue;
+                }
+
+                type.Folder = Path.Combine("FarmTypes", Path.GetFileName(choice));
+                FarmType.register(type);
+                Log.info("\tFarm type: " + type.Name + " (" + type.ID + ")");
+            }
+        }
+    }
+}
